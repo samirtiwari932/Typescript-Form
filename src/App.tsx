9bdfdedda1866fd944eb83275ec342cc2bc8d8eb
Sameer <samirtiwari567@gmail.com>
2023-03-14 16:50:33 +0545

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { FC,useState } from "react";
+
+// Defining an interface for the form data
+interface FormData {
+  name: string;
+  email: string;
+  address:string;
+  number : [];
+  
+}
+
+
+const App: FC = () => {
+  
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    address:"",
+    number:[]
+  });
+
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    
+    e.preventDefault();
+    
+    console.log(formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Submit Form in TypeScript</h1>
+      <form className="" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+        <br /><br />
+        <label htmlFor="address">Address:</label>
+        <input type="text" id="address" name="address" value={formData.address} onChange={handleChange} />
+        <br /><br />
+        <label htmlFor="address">Mobile Number:</label>
+        <input type="number" id="number" name="number" value={formData.number} onChange={handleChange} />
+        <br /><br />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+        <br /><br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
